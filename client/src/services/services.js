406c96a3cdc4df917dc9149c37f472122bcf31e0
guscua2002 <guscua2002@yahoo.com>
2023-06-03 19:55:20 -0400

    fin

--- client/src/services/services.js
+++ client/src/services/services.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-export const getAllPets = () => axios.get('http://api/users/');
+export const getAllPets = () => axios.get('api/users/');
 
-export const getOnePet = (id) => axios.get(`http://api/users/${id}`); 
+export const getOnePet = (id) => axios.get(`api/users/${id}`); 
 
-export const createPet = (userInfo) => axios.post('http://api/users/new', userInfo);
+export const createPet = (userInfo) => axios.post('api/users/new', userInfo);
 
-export const updatePet = (id, task) => axios.put(`http://api/users/update/${id}`, task);
+export const updatePet = (id, task) => axios.put(`api/users/update/${id}`, task);
 
-export const deletePet = (id) => axios.delete(`http://api/users/delete/${id}`); 
+export const deletePet = (id) => axios.delete(`api/users/delete/${id}`); 
 
 
 
--- client/src/utils/ValidaForm.js
+++ client/src/utils/ValidaForm.js
@@ -7,9 +7,9 @@ const messageThree = "Descripcion debe tener entre 3 y 40 caracteres comunes";
 
 
    export const petsSchema = yup.object().shape({                                                                           
-    petname: yup.string().typeError(messagOne).min(5,{ message: messagOne }).max(40,{ message: messagOne }).matches(/^(?!\s)[a-zA-Z0-9_-]*[^$%&()/#"'!*?]*$/,{ message: messagOne }).required({message: messagOne}),
-    pettype: yup.string().typeError(messageTwo).min(5,{ message: messageTwo }).max(40,{ message: messageTwo }).matches(/^(?!\s)[a-zA-Z0-9_-]*[^$%&()/#"'!*?]*$/,{ message: messageTwo }).required({message: messageTwo}),
-    petdescription: yup.string().typeError(messageThree).min(5,{ message: messageThree }).max(40,{ message: messageThree }).matches(/^(?!\s)[a-zA-Z0-9_-]*[^$%&()/#"'!*?]*$/,{ message: messageThree }).required({message: messageThree}),
+    petname: yup.string().typeError(messagOne).min(3,{ message: messagOne }).max(40,{ message: messagOne }).matches(/^(?!\s)[a-zA-Z0-9_-]*[^$%&()/#"'!*?]*$/,{ message: messagOne }).required({message: messagOne}),
+    pettype: yup.string().typeError(messageTwo).min(3,{ message: messageTwo }).max(40,{ message: messageTwo }).matches(/^(?!\s)[a-zA-Z0-9_-]*[^$%&()/#"'!*?]*$/,{ message: messageTwo }).required({message: messageTwo}),
+    petdescription: yup.string().typeError(messageThree).min(3,{ message: messageThree }).max(40,{ message: messageThree }).matches(/^(?!\s)[a-zA-Z0-9_-]*[^$%&()/#"'!*?]*$/,{ message: messageThree }).required({message: messageThree}),
    
     });
 
