4714cffe7c83f270b11da719e7fb29d39559b16e
guscua2002 <guscua2002@yahoo.com>
2023-06-04 15:36:34 -0400

    alfin

--- client/src/services/services.js
+++ client/src/services/services.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-export const getAllPets = () => axios.get('api/users/');
+export const getAllPets = () => axios.get('/api/users/');
 
-export const getOnePet = (id) => axios.get(`api/users/${id}`); 
+export const getOnePet = (id) => axios.get(`/api/users/${id}`); 
 
-export const createPet = (userInfo) => axios.post('api/users/new', userInfo);
+export const createPet = (userInfo) => axios.post('/api/users/new', userInfo);
 
-export const updatePet = (id, task) => axios.put(`api/users/update/${id}`, task);
+export const updatePet = (id, task) => axios.put(`/api/users/update/${id}`, task);
 
-export const deletePet = (id) => axios.delete(`api/users/delete/${id}`); 
+export const deletePet = (id) => axios.delete(`/api/users/delete/${id}`); 
 
 
 
